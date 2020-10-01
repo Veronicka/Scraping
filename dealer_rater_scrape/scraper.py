988a5302b2910161be3ad8d27f6267c319b838bc
Veronicka <veronicka_ms@hotmail.com>
2020-10-01 18:51:39 -0300

    fix scraper file

--- dealer_rater_scrape/scraper.py
+++ dealer_rater_scrape/scraper.py
@@ -9,7 +9,7 @@ URL_DEALER_RATER = (
     "McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685"
 )
 
-PAGES = 5
+PAGES_LIMIT = 5
 
 
 def request_url():
@@ -18,7 +18,7 @@ def request_url():
     list_reviews = []
     total = 0
     session = requests.Session()
-    for page in reversed(range(PAGES)):
+    for page in reversed(range(PAGES_LIMIT)):
         response = session.get(f"{URL_DEALER_RATER}/page{page+1}")
         reviews = scraping_response(response)
         list_reviews.append(reviews)
@@ -40,22 +40,6 @@ def scraping_response(response):
     return reviews
 
 
-def get_review_ratings(review_wrapper):
-    review_ratings = {}
-    for ratings in review_wrapper.select(".review-ratings-all"):
-        for rating in ratings.select(".tr"):
-            name_rating = rating.select_one(".lt-grey.small-text.td")
-            number_stars = rating.select_one(".rating-static-indv")
-            if number_stars:
-                number_stars = int(number_stars.attrs["class"][1][-2])
-            else:
-                number_stars = rating.select_one(".td.small-text.boldest").text.replace(
-                    " ", ""
-                )
-            review_ratings[name_rating.text] = number_stars
-    return review_ratings
-
-
 def build_review(review_wrapper):
     review = {}
     title = review_wrapper.h3
@@ -67,3 +51,21 @@ def build_review(review_wrapper):
     review["text"] = text.text
     review["ratings"] = get_review_ratings(review_wrapper)
     return review
+
+
+def get_review_ratings(review_wrapper):
+    review_ratings = {}
+    review_ratings_all = review_wrapper.select(".review-ratings-all")
+    for ratings in review_ratings_all:
+        review_rating = ratings.select(".tr")
+        for rating in review_rating:
+            rating_name = rating.select_one(".lt-grey.small-text.td")
+            number_of_stars = rating.select_one(".rating-static-indv")
+            if number_of_stars:
+                number_of_stars = int(number_of_stars.attrs["class"][1][-2])
+            else:
+                number_of_stars = rating.select_one(
+                    ".td.small-text.boldest"
+                ).text.replace(" ", "")
+            review_ratings[rating_name.text] = number_of_stars
+    return review_ratings
